257921e923867c8bd92cba6c1c5d8081a6e5bac3
Enz0Gabriel <enzogabrielcarvalho123@gmail.com>
2025-12-17 17:57:35 -0300

    Consertando app css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from './app.module.css'
 import {Header} from './components/Header'
+import {Tip} from './components/Tip'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestart} />
+
+        <Tip tip="Lembre-se de revisar seu código antes de enviar!" />
       </main>
     </div>
   )
